b6bb6fec85c12b955c33e1f123a5a27f0b53aa0e
IgorMarchenko1 <98159771+IgorMarchenko1@users.noreply.github.com>
2022-02-11 16:55:18 +0300

    Update taskone.js

--- taskone.js
+++ taskone.js
@@ -16,3 +16,25 @@ function compare(a, b, c) {
 
 let results = compare(a, b, c);
 console.log(results);
+
+
+//-----------------------------------
+
+let ourArray = [3, 5, 4];
+
+let a = ourArray[0];
+let b = ourArray[1];
+let c = ourArray[2];
+
+
+function compare(a, b, c) {
+    if (a < b && b > c) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+let results = compare(a, b, c);
+console.log(results);
